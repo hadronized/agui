84208bba1137f2b5a78f0dc1bcbc5893b27b9e3f
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-07-13 15:22:33 +0200

    Update Widget.hs

--- src/Graphics/UI/AGUI/Core/Widget.hs
+++ src/Graphics/UI/AGUI/Core/Widget.hs
@@ -26,10 +26,15 @@ import Graphics.UI.AGUI.Core.Placement ( Placement )
 -- A 'Widget' is a 'Functor', meaning that you can change its type as long as
 -- you can still use it later on. For instance, changing the type of a 'Widget'
 -- might prevent you from rendering it.
---
--- If you change the type of a 'Widget', you cannot
 newtype Widget a = Widget { unWidget :: IO (El a) } deriving (Functor)
 
+-- |Create a new 'Widget'.
+--
+-- You get two objects by creating a new 'Widget'. You get a @'Widget' a@ and a
+-- @'Trigger' ('El' a)@. The former represents the 'Widget' itself and can be
+-- used to compose the /GUI/. The latter is used to interact with the reactive
+-- 'El' value stored in the 'Widget'. You can use the 'Trigger' to change the
+-- value of the 'Widget'.
 newWidget :: (MonadIO m)
           => a
           -> Margin
